7b5c8664283b5728aa2c4ad0f9ffd3b52a2ffd54
Julio Davila <juliodacer@gmail.com>
2022-11-07 13:22:16 -0500

    validando formulario con formik en registro

--- screens/RegisterScreen.jsx
+++ screens/RegisterScreen.jsx
@@ -3,110 +3,151 @@ import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, Vi
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 import { auth } from '../firebase';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup'
+
 
 const RegisterScreen = ({ navigation }) => {
 
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [confirmPassword, setConfirmPassword] = React.useState("");
-  const [validationMessage, setValidationMessage] = React.useState("");
+  const SignUpSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Correo inválido')
+      .required('Por favor ingrese un correo'),
+    password: Yup.string()
+      .min(8, 'Mínimo 8 caracteres')
+      .required('Por favor ingrese una contraseña')
+      .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/,
+        'Minimo 8 caracteres, una Mayúscula, una minúscula, un número'
+      )
+    ,
+    confirmPassword: Yup.string()
+      .min(8, 'Mínimo 8 caracteres')
+      .oneOf([Yup.ref('password')], 'Su contraseña no coincide')
+      .required('Por favor confirme su contraseña')
+    
+  })
 
-  let validateAndSet = (value, valueToCompare, setValue) => {
-    if (value !== valueToCompare) {
-      setValidationMessage("Contraseñas no coinciden");
-    } else {
-      setValidationMessage("");
-    }
-
-    setValue(value);
-  };
-
-  let signUp = () => {
-    if (password === confirmPassword) {
-      createUserWithEmailAndPassword(auth, email, password)
+  let signUp = (values) => {
+    if (values.password === values.confirmPassword) {
+      createUserWithEmailAndPassword(auth, values.email, values.password)
         .then((userCredential) => {
           sendEmailVerification(auth.currentUser);
           navigation.navigate("Home", { user: userCredential.user });
         })
         .catch((error) => {
-          setValidationMessage(error.message);
+          //setValidationMessage(error.message);
+          console.log(error.message);
         });
     }
   }
 
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : null}
-      keyboardVerticalOffset={60}
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }}
+      validationSchema={SignUpSchema}
+      onSubmit={values => {signUp(values)}}
     >
-      <View style={styles.inputContainer} >
 
-        <View style={styles.titleContainer} >
-          <Text style={styles.title}>REGISTRARME</Text>
-        </View>
+      {({ values, errors, touched, handleChange, setFieldTouched, isValid, handleSubmit }) => (
 
-        <Text style={styles.textValidationMessage}>{validationMessage}</Text>
-    
-        <View style={styles.inputTextContainerGroup}>
-          <View style={styles.inputTextContainer} >
 
-            <Icon name="person" style={styles.iconInput} />
 
-            <TextInput
-              placeholder='Usuario'
-              style={styles.input}
-              value={email}
-              onChangeText={setEmail}
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior={Platform.OS === "ios" ? "padding" : null}
+          keyboardVerticalOffset={60}
+        >
+          <View style={styles.inputContainer} >
 
-            />
+            <View style={styles.titleContainer} >
+              <Text style={styles.title}>REGISTRARME</Text>
+            </View>
+
+            <View style={styles.inputTextContainerGroup}>
+              <View style={styles.inputTextContainer} >
+
+                <Icon name="person" style={styles.iconInput} />
+
+                <TextInput
+                  autoCapitalize={false}
+                  placeholder='Usuario'
+                  style={styles.input}
+                  value={values.email}
+                  onChangeText={handleChange('email')}
+                  onBlur={() => setFieldTouched('email')}
+
+                />
+              </View>
+
+              {touched.email && errors.email && (
+                <Text style={styles.textValidationMessage}>{errors.email}</Text>
+              )}
+
+              <View style={styles.inputTextContainer} >
+
+                <Icon name="lock" style={styles.iconInput} />
+
+                <TextInput
+                  placeholder='Contraseña'
+                  autoCapitalize={false}
+                  style={styles.input}
+                  value={values.password}
+                  onChangeText={handleChange('password')}
+                  secureTextEntry
+                  onBlur={() => setFieldTouched('password')}
+                />
+              </View>
+
+              {touched.password && errors.password && (
+                <Text style={styles.textValidationMessage}>{errors.password}</Text>
+              )}
+
+              <View style={styles.inputTextContainer} >
+
+                <Icon name="lock" style={styles.iconInput} />
+
+                <TextInput
+                  placeholder='Confirmar Contraseña'
+                  style={styles.input}
+                  autoCapitalize={false}
+                  secureTextEntry
+                  value={values.confirmPassword}
+                  onChangeText={handleChange('confirmPassword')}
+                  onBlur={() => setFieldTouched('confirmPassword')}
+                />
+              </View>
+              {touched.confirmPassword && errors.confirmPassword && (
+                <Text style={styles.textValidationMessage}>{errors.confirmPassword}</Text>
+              )}
+            </View>
+
+
+            <View style={[styles.buttonContainer, { backgroundColor: isValid ? '#5CB85C' : '#A5C9CA' }]}>
+              <TouchableOpacity 
+                onPress={handleSubmit}
+                disabled={!isValid}
+                style={styles.button}
+              >
+                <Text style={styles.textButton}>REGISTRAR</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.goToContainer}>
+              <TouchableOpacity onPress={() => navigation.navigate('Login')}
+              >
+                <Text style={styles.textGoTo}>Cancelar</Text>
+              </TouchableOpacity>
+            </View>
+
           </View>
 
-          <View style={styles.inputTextContainer} >
-
-            <Icon name="lock" style={styles.iconInput} />
-
-            <TextInput
-              placeholder='Contraseña'
-              style={styles.input}
-              value={password}
-              onChangeText={(value) => validateAndSet(value, confirmPassword, setPassword)}
-              secureTextEntry
-            />
-          </View>
-
-          <View style={styles.inputTextContainer} >
-
-            <Icon name="lock" style={styles.iconInput} />
-
-            <TextInput
-              placeholder='Confirmar Contraseña'
-              style={styles.input}
-              secureTextEntry
-              value={confirmPassword}
-              onChangeText={(value) => validateAndSet(value, password, setConfirmPassword)}
-            />
-          </View>
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={signUp}
-            style={styles.button}
-          >
-            <Text style={styles.textButton}>REGISTRAR</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.goToContainer}>
-          <TouchableOpacity onPress={() => navigation.navigate('Login')}
-          >
-            <Text style={styles.textGoTo}>Cancelar</Text>
-          </TouchableOpacity>
-        </View>
-
-      </View>
-
-    </KeyboardAvoidingView>
+        </KeyboardAvoidingView>
+      )}
+    </Formik>
   )
 }
 
@@ -136,10 +177,10 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontWeight: '700'
   },
-  
+
   textValidationMessage: {
     color: 'red',
-    alignSelf: 'center'
+    alignSelf: 'flex-start'
   },
   inputTextContainerGroup: {
     marginHorizontal: 30,
@@ -163,7 +204,7 @@ const styles = StyleSheet.create({
   },
 
   buttonContainer: {
-    backgroundColor: '#5CB85C',
+    //backgroundColor: '#5CB85C',
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
